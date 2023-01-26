4db7d42f683d59cd5cb586c2ed029df954a3ce4a
Alycat <Arc183@student.aru.ac.uk>
2023-01-26 13:42:37 +0000

    Swapping

--- Program.cs
+++ Program.cs
@@ -3,7 +3,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.ReadKey(true);
+string num1, num2, temp;
+
+        Console.WriteLine("Input first number");
+        num1 = Console.ReadLine();
+        Console.WriteLine("Input Second number");
+        num2 = Console.ReadLine();
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+        Console.WriteLine("After Swapping");
+        Console.WriteLine($"First Number : {num1}");
+        Console.WriteLine($"Second Number : {num2}");
+
+        Console.ReadKey(true); 
     }
 }
